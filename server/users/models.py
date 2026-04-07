db00d079eb07b3cac90d29b9bc275c13c81bcab9
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-08 02:07:24 +0500

    update

--- server/users/models.py
+++ server/users/models.py
@@ -1,11 +1,13 @@
 # ===================== PYTHON =================
 import uuid
 from random import randint
+from datetime import timedelta
 
 # ===================== DJANGO =================
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import identify_hasher
+from django.utils import timezone
 
 # =================== SHARED ====================
 from shared.models import BaseModel
@@ -17,10 +19,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class AuthStatus(models.TextChoices):
     NEW = "new", "NEW"
     CODE_VERIFED = "code_verified", "Code veriftied"
-    DONE = (
-        "done",
-        "Done",
-    )
+    DONE = "done", "Done"
     PHOTO_DONE = "photo_done", "Photo Done"
 
 
@@ -39,9 +38,9 @@ def is_hashed(password):
 
 class User(BaseModel, AbstractUser):
     auth_status = models.CharField(
-        max_length=10, choices=AuthStatus.choices, default=AuthStatus.NEW
+        max_length=13, choices=AuthStatus.choices, default=AuthStatus.NEW
     )
-    auth_type = models.CharField(max_length=9, choices=AuthStatus.choices)
+    auth_type = models.CharField(max_length=13, choices=AuthStatus.choices)
     photo = models.ImageField(upload_to="user/profile", blank=True, null=True)
     email = models.EmailField(max_length=128, blank=True, null=True, unique=True)
     phone_number = models.CharField(max_length=16, blank=True, null=True, unique=True)
@@ -68,7 +67,7 @@ class User(BaseModel, AbstractUser):
             normilze_email = self.email.lower().strip()
             self.email = normilze_email
 
-    def check_user_password(self, raw_password):
+    def check_user_password(self):
         if not self.password:
             temp_username = f"wallet-{uuid.uuid4().__str__().split("-")[-1]}"
             self.password = temp_username
@@ -102,9 +101,35 @@ class User(BaseModel, AbstractUser):
         super().save(*args, *kwargs)
 
 
+EXPIRE_EMAIL = 2
+EXPIRE_PHONE = 5
+
+
 class UserConfirmation(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="verify_code")
     code = models.CharField(max_length=4)
     auth_type = models.CharField(max_length=9, choices=AuthType.choices)
     expiration_date = models.DateTimeField(null=True, blank=True)
     is_confirmed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            if self.auth_type == AuthType.VIA_Phone:
+                self.expiration_date = timezone.now() + timedelta(minutes=EXPIRE_PHONE)
+            elif self.auth_type == AuthType.VIA_EMAIL:
+                self.expiration_date = timezone.now() + timedelta(minutes=EXPIRE_EMAIL)
+
+        super().save(*args, **kwargs)
+
+    def is_expired(self) -> bool:
+        if self.expiration_date is None:
+            return True
+        return timezone.now() > self.expiration_date
+
+    def can_verify(self) -> bool:
+        if self.is_confirmed:
+            return False
+        if self.is_expired():
+            return False
+
+        return True
