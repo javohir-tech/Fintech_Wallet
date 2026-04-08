f4e913be177308c29b640b56d7a84cd21b416f2b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-08 16:14:08 +0500

    update

--- server/users/models.py
+++ server/users/models.py
@@ -1,34 +1,22 @@
-# ===================== PYTHON =================
+# ==================== PYTHON =========================
 import uuid
 from random import randint
 from datetime import timedelta
 
-# ===================== DJANGO =================
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import identify_hasher
-from django.utils import timezone
-
-# =================== SHARED ====================
+# ==================== SHARED =========================
 from shared.models import BaseModel
 
-# =================== REST FRAMEWORK ============
+# ==================== DJANGO =========================
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import identify_hasher
+
+# ==================== REST FRAMEWORK ==================
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-class AuthStatus(models.TextChoices):
-    NEW = "new", "NEW"
-    CODE_VERIFED = "code_verified", "Code veriftied"
-    DONE = "done", "Done"
-    PHOTO_DONE = "photo_done", "Photo Done"
-
-
-class AuthType(models.TextChoices):
-    VIA_EMAIL = "via_email", "Via Email"
-    VIA_Phone = "via_phone", "Via Phone"
-
-
-def is_hashed(password):
+def is_hashing(password):
     try:
         identify_hasher(password)
         return True
@@ -36,46 +24,54 @@ def is_hashed(password):
         return False
 
 
+class AuthStatus(models.TextChoices):
+    NEW = "new", "New"
+    VERIFIED = "verified", "Verified"
+    DONE = "done", "Done"
+    PHOTO_DONE = "photo_done", "Photo Done"
+
+
+class AuthType(models.TextChoices):
+    VIA_EMAIL = "via_email", "VIA Email"
+    VIA_PHONE = "via_phone", "VIA Phone"
+
+
 class User(BaseModel, AbstractUser):
     auth_status = models.CharField(
-        max_length=13, choices=AuthStatus.choices, default=AuthStatus.NEW
+        max_length=10, choices=AuthStatus.choices, default=AuthStatus.NEW
     )
-    auth_type = models.CharField(max_length=13, choices=AuthStatus.choices)
-    photo = models.ImageField(upload_to="user/profile", blank=True, null=True)
-    email = models.EmailField(max_length=128, blank=True, null=True, unique=True)
-    phone_number = models.CharField(max_length=16, blank=True, null=True, unique=True)
+    auth_type = models.CharField(
+        max_length=10   , choices=AuthStatus.choices, default=AuthType.VIA_EMAIL
+    )
+    email = models.CharField(max_length=128, null=True, unique=True)
+    phone_number = models.CharField(max_length=128, null=True, unique=True)
+    avatar = models.ImageField(upload_to="users/avatars/", null=True)
 
-    # ///////////// CREATE  VERIFY CODE //////////////////
-    def create_verify_code(self, auth_type):
-        code = "".join([str(randint(0, 9)) for _ in range(4)])
+    # =============== CREATE CODE ===================
+    def create_code(self, auth_type):
+        code = "".join([str(randint(0, 10000) % 10) for _ in range(4)])
         UserConfirmation.objects.create(user=self, code=code, auth_type=auth_type)
         return code
 
-    def __str__(self):
-        return self.username
-
     def check_username(self):
         if not self.username:
-            temp_username = f"wallet-{uuid.uuid4().__str__().split("-")[-1]}"
-
+            temp_username = f"temp_username_{uuid.uuid4().__str__().strip("-")[-1]}"
             while User.objects.filter(username=temp_username).exists():
                 temp_username = f"{temp_username}{randint(1, 9)}"
+
             self.username = temp_username
 
     def check_email(self):
         if self.email:
-            normilze_email = self.email.lower().strip()
-            self.email = normilze_email
+            normalize_email = self.email.lower().strip()
+            self.email = normalize_email
 
     def check_user_password(self):
         if not self.password:
-            temp_username = f"wallet-{uuid.uuid4().__str__().split("-")[-1]}"
-            self.password = temp_username
+            temp_password = f"temp_password_{uuid.uuid4().__str__().strip("-")[-1]}"
+            self.password = temp_password
 
-    def hashing_password(self):
-        if not is_hashed(self.password):
-            self.set_password(self.password)
-
+    # ==================== CREATE TOKEN ====================
     def token(self):
         refresh = RefreshToken.for_user(self)
         return {
@@ -83,22 +79,24 @@ class User(BaseModel, AbstractUser):
             "refresh_token": str(refresh),
         }
 
+    def password_hashing(self):
+        if not is_hashing(self.password):
+            self.password = self.set_password(self.password)
+
     def clean(self):
         self.check_username()
-        self.check_user_password()
-        self.hashing_password()
         self.check_email()
+        self.check_user_password()
+        self.password_hashing()
 
     def save(self, *args, **kwargs):
         if self._state.adding:
             self.clean()
         else:
-            self.email()
+            self.check_email()
 
-        if self.password and not is_hashed(self.password):
-            self.set_password(self.password)
-
-        super().save(*args, *kwargs)
+        if self.password and not is_hashing(self.password):
+            self.password = self.set_password(self.password)
 
 
 EXPIRE_EMAIL = 2
@@ -106,30 +104,30 @@ EXPIRE_PHONE = 5
 
 
 class UserConfirmation(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="verify_code")
     code = models.CharField(max_length=4)
-    auth_type = models.CharField(max_length=9, choices=AuthType.choices)
-    expiration_date = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="verify_code")
+    expired_time = models.DateTimeField()
+    auth_type = models.CharField(max_length=10, choices=AuthType.choices)
     is_confirmed = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         if self._state.adding:
-            if self.auth_type == AuthType.VIA_Phone:
-                self.expiration_date = timezone.now() + timedelta(minutes=EXPIRE_PHONE)
-            elif self.auth_type == AuthType.VIA_EMAIL:
-                self.expiration_date = timezone.now() + timedelta(minutes=EXPIRE_EMAIL)
+            if self.auth_type == AuthType.VIA_EMAIL:
+                self.expired_time = timezone.now() + timedelta(minutes=EXPIRE_EMAIL)
+            elif self.auth_type == AuthType.VIA_PHONE:
+                self.expired_time = timezone.now() + timedelta(minutes=EXPIRE_PHONE)
 
         super().save(*args, **kwargs)
 
-    def is_expired(self) -> bool:
-        if self.expiration_date is None:
+    def is_expired(self):
+        if self.expired_time is None:
             return True
-        return timezone.now() > self.expiration_date
+        return timezone.now() > self.expired_time
 
-    def can_verify(self) -> bool:
+    def can_confirmed(self):
         if self.is_confirmed:
             return False
-        if self.is_expired():
+        if self.is_expired:
             return False
 
         return True
